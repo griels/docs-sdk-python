607309eb1f1e699f61b105f1e4b3d30696d5335a
Ellis Breen <ellis.breen@couchbase.com>
2020-01-21 16:50:04 +0000

    Migration guide draft:
    Start Python file to pull in to migration doc
    Finished first pass of Java to Python port
    Do Admin->Bucket/UserManager and BucketManager->View/QueryIndexManager translation tables.
    Rename examples to example to match Java structure
    Correct nav bar
    Remove irrelevant material

---
+++ modules/project-docs/example/migrating.py
@@ -0,0 +1,215 @@
+from datetime import timedelta
+from couchbase.cluster import Cluster
+from couchbase.collection import GetOptions
+class Migrating(object):
+    pass
+
+#tag::timeoutbuilder[]
+# SDK 3 equivalent
+cluster=Cluster("couchbases://10.192.1.104")
+collection=cluster.bucket("default").default_collection()
+collection.timeout=5
+#end::timeoutbuilder[]
+
+# not applicable
+#natag::shutdown[]
+# ClusterEnvironment env = ClusterEnvironment.create();
+# Cluster cluster = Cluster.connect(
+#     "127.0.0.1",
+#                   // pass the custom environment through the cluster options
+# clusterOptions("user", "pass").environment(env)
+# );
+#
+# // first disconnect, then shutdown the environment
+# cluster.disconnect();
+# env.shutdown();
+#naend::shutdown[]
+
+
+#natag::sysprops[]
+# not applicable
+# Will set the max http connections to 23
+# System.setProperty("com.couchbase.env.io.maxHttpConnections", "23");
+# Cluster.connect("127.0.0.1", "user", "pass");
+#
+# #This is equivalent to
+# ClusterEnvironment env = ClusterEnvironment \
+#     .builder() \
+#     .ioConfig(IoConfig.maxHttpConnections(23)) \
+#     .build();
+#naend::sysprops[]
+
+from couchbase.cluster import *
+from couchbase_core.cluster import PasswordAuthenticator
+from couchbase_v2 import COMPRESS_INOUT
+#tag::connstr[]
+# Will set the compression type to inout
+Cluster.connect(
+    "127.0.0.1?compression=inout",ClusterOptions(PasswordAuthenticator(
+    "user",
+    "pass")))
+
+# This is equivalent to
+collection.compression = COMPRESS_INOUT
+#end::connstr[]
+
+#natag::rbac[]
+# add convenience overload when available
+Cluster.connect("127.0.0.1", PasswordAuthenticator("username", "password"))
+#naend::rbac[]
+
+#tag::rbac-full[]
+Cluster.connect(
+    "127.0.0.1",
+    ClusterOptions(PasswordAuthenticator("username", "password")))
+#end::rbac-full[]
+
+from couchbase_core.cluster import  CertAuthenticator
+import os
+#tag::certauth[]
+cert_dir=os.path.join(os.path.curdir,"cert_dir")
+
+Cluster.connect("127.0.0.1", ClusterOptions(
+    CertAuthenticator(cert_path="cert.pem",
+                      key_path="key.crt",
+                      trust_store_path="trust_store.pem"
+)))
+#end::certauth[]
+
+#tag::simpleget[]
+cluster = Cluster.connect("127.0.0.1", ClusterOptions(PasswordAuthenticator("user", "pass")))
+bucket = cluster.bucket("travel-sample")
+collection = bucket.default_collection()
+
+get_result = collection.get("airline_10")
+
+#end::simpleget[]
+
+cluster = Cluster.connect("127.0.0.1", ClusterOptions(PasswordAuthenticator("user", "pass")))
+bucket = cluster.bucket("travel-sample")
+collection = bucket.default_collection()
+
+#tag::upsertandget[]
+upsert_result = collection.upsert("mydoc-id", {})
+get_result = collection.get("mydoc-id")
+#end::upsertandget[]
+
+import couchbase_tests.base
+
+#natag::rawjson[]
+# TODO: update when implemented
+from couchbase.collection import UpsertOptions
+from couchbase_core.transcoder import Transcoder
+
+class RawJSONTranscoder(Transcoder):
+    pass
+
+content = "{}".encode("UTF_8")
+upsert_result = collection.upsert(
+    "mydoc-id",
+    content,
+    UpsertOptions(transcoder=RawJSONTranscoder))
+
+#naend::rawjson[]
+
+class TimeoutTest(couchbase_tests.base.CollectionTestCase):
+
+    def test_customtimeout(self):
+#tag::customtimeout[]
+# SDK 3 custom timeout
+        get_result = collection.get(
+            "mydoc-id",
+            GetOptions(timeout=timedelta(seconds=5)))
+        #tag::test[]
+        self.assertEquals("fish",get_result.content_as[str])
+        #end::test[]
+#end::customtimeout[]
+
+#tag::querysimple[]
+# SDK 3 simple query
+query_result = cluster.query("select * from `travel-sample` limit 10")
+for value in query_result:
+    #...
+    pass
+#end::querysimple[]
+
+
+# tag::queryparameterized[]
+# SDK 3 named parameters
+cluster.query(
+    "select * from bucket where type = $type",
+    QueryOptions(named_parameters={"type": "airport"}))
+
+# SDK 3 positional parameters
+cluster.query(
+    "select * from bucket where type = $1",
+    QueryOptions(positional_parameters=["airport"]))
+#end::queryparameterized[]
+
+#tag::analyticssimple[]
+# SDK 3 simple analytics query
+analytics_result = cluster.analytics_query("select * from dataset")
+for value in analytics_result:
+    #...
+    pass
+#end::analyticssimple[]
+
+#natag::analyticsparameterized[]
+# SDK 3 named parameters for analytics
+# TODO: enable when implemented, still offering old style *args, **kwargs interface
+cluster.analytics_query(
+        "select * from dataset where type = $type",
+        AnalyticsOptions(named_parameters={"type": 'airport'}))
+
+# SDK 3 positional parameters for analytics
+cluster.analytics_query(
+    "select * from dataset where type = $1",
+    AnalyticsOptions(position_paramters=["airport"]))
+#naend::analyticsparameterized[]
+
+
+#tag::analyticsparameterized_args_kwargs[]
+# SDK 3 named parameters for analytics
+# TODO: enable when implemented, still offering old style *args, **kwargs interface
+cluster.analytics_query(
+    "select * from dataset where type = $type",
+    type='airport')
+
+# SDK 3 positional parameters for analytics
+cluster.analytics_query(
+    "select * from dataset where type = $1",
+    ["airport"])
+#naend::analyticsparameterized_args_kwargs[]
+
+#tag::searchsimple[]
+# SDK 3 search query
+search_result = cluster.search_query(
+    "indexname",
+    "airports",
+    SearchOptions(timeout=timedelta(seconds=2),limit=5,fields=("a", "b", "c")))
+for row in search_result:
+    # ...
+    pass
+#end::searchsimple[]
+
+from couchbase_core.fulltext import Facet, TermFacet, DateFacet, NumericFacet
+#natag::searchcheck_args_kwargs[]
+search_result = cluster.search_query(
+        "myindex",
+        facets={'searchstring':Facet()}.queryString("searchstring"))
+if search_result.metadata().error_count()==0:
+    # no errors present, so full data got returned
+    pass
+#naend::searchcheck_args_kwargs[]
+
+from couchbase.bucket import ViewOptions
+#tag::viewquery[]
+# SDK 3 view query
+view_result = bucket.view_query(
+    "design",
+    "view",
+    ViewOptions(limit=5,skip=2,timeout=timedelta(seconds=10)))
+for row in view_result:
+    # ...
+    pass
+#end::viewquery[]
